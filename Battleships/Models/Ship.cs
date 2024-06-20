bdb7ae5eebe8921de97e977164618edb33fc3d57
Komputer Domowy <KrzychuK1211@gmail.com>
2024-06-20 15:11:44 +0200

    Changing name of properties so it matches the frontend one

--- Battleships/Models/Ship.cs
+++ Battleships/Models/Ship.cs
@@ -3,6 +3,6 @@
     public class Ship
     {
         public string Name { get; set; } = string.Empty;
-        public List<string> Fields { get; set; } = new List<string>();
+        public List<string> BoardFields { get; set; } = new List<string>();
     }
 }
