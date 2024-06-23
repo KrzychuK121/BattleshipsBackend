57a115b0671039a4471c3dbe5f5b9b82b3c2d3ac
Komputer Domowy <KrzychuK1211@gmail.com>
2024-06-23 18:26:09 +0200

    Created new static method that makes copy of list of ships so it can be used to remove hitted by user cells

--- Battleships/Models/Ship.cs
+++ Battleships/Models/Ship.cs
@@ -4,5 +4,28 @@
     {
         public string Name { get; set; } = string.Empty;
         public List<string> BoardFields { get; set; } = new List<string>();
+
+        public static List<Ship> CopyShips(List<Ship> toCopy)
+        {
+            var copy = new List<Ship>();
+
+            foreach (var ship in toCopy)
+            {
+                Ship copyOfShip = new Ship(){ 
+                    Name = ship.Name,
+                    BoardFields = new List<string>()
+                };
+                
+                foreach(var field in ship.BoardFields)
+                {
+                    copyOfShip.BoardFields.Add(field);
+                }
+
+                copy.Add(copyOfShip);
+            }
+            
+
+            return copy;
+        }
     }
 }
